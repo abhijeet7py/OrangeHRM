5daf58d1bb4389837b5d676c2a9dfbba9c156634
Abhijeet Jathar <abhijeetjathar77@gmail.com>
2025-10-14 23:19:59 +0530

    Login Tests

--- tests/test/test_orange_hrm_login.py
+++ tests/test/test_orange_hrm_login.py
@@ -12,11 +12,11 @@ from tests.pageObjects.pom.loginPage import LoginPage
 
 @pytest.fixture()
 def setup():
-    # options = Options()
-    # driver =webdriver.Chrome(options = options)
-    # options.add_argument("--start-maximized")
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+    options = Options()
+    driver =webdriver.Chrome(options = options)
+    options.add_argument("--headless")
+    # driver = webdriver.Chrome()
+    # driver.maximize_window()
     driver.get(Constants.login_url())
     return driver
 
@@ -35,8 +35,44 @@ def test_orangehrm_login_positive(setup):
 @allure.epic("OrangeHRM Login test")
 @allure.feature("TC#1- OrangeHRM Login test- Negative")
 @pytest.mark.negative
-def test_orangehrm_login_negative(setup):
+def test_orangehrm_login_negative_1(setup):
     login_page = LoginPage(driver=setup)
     login_page.login_to_hrm(usr="Ad",pwd="ad")
+    error = login_page.get_error()
+    assert error == "Invalid credentials"
+
+@allure.epic("OrangeHRM Login test")
+@allure.feature("TC#2- OrangeHRM Login test- Negative")
+@pytest.mark.negative
+def test_orangehrm_login_negative_2(setup):
+    login_page = LoginPage(driver=setup)
+    login_page.login_to_hrm(usr="",pwd="admin123")
+    req_error = login_page.get_usr_req()
+    assert req_error == "Required"
+
+@allure.epic("OrangeHRM Login test")
+@allure.feature("TC#3- OrangeHRM Login test- Negative")
+@pytest.mark.negative
+def test_orangehrm_login_negative_3(setup):
+    login_page = LoginPage(driver=setup)
+    login_page.login_to_hrm(usr="Admin",pwd="")
+    req_error = login_page.get_pass_req()
+    assert req_error == "Required"
+
+@allure.epic("OrangeHRM Login test")
+@allure.feature("TC#4- OrangeHRM Login test- Negative")
+@pytest.mark.negative
+def test_orangehrm_login_negative_4(setup):
+    login_page = LoginPage(driver=setup)
+    login_page.login_to_hrm(usr="Admin",pwd="sd")
+    error = login_page.get_error()
+    assert error == "Invalid credentials"
+
+@allure.epic("OrangeHRM Login test")
+@allure.feature("TC#5- OrangeHRM Login test- Positive")
+@pytest.mark.negative
+def test_orangehrm_login_negative_5(setup):
+    login_page = LoginPage(driver=setup)
+    login_page.login_to_hrm(usr="Adm",pwd="admin123")
     error = login_page.get_error()
     assert error == "Invalid credentials"
